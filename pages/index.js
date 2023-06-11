a3929f8d0da0674554f80c5c0e1b873a2bfb4fd8
masaru suzuki <masaru.suzuki.yamagata@gmail.com>
2023-06-12 05:54:24 +0900

    add:Homeのコンテンツ部分をコーディhング

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 import utilStyles from '../styles/utils.module.css';
+import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
 import Layout from '../components/layout';
@@ -10,6 +10,75 @@ export default function Home() {
     <Layout>
       <section>
         <p className={utilStyles.headingMd}>プロフィールの文章です。</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2>📝エンジニアのブログ</h2>
+        <div className={styles.grid}>
+          <article>
+            <Link href="/">
+              <img
+                src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+                alt=""
+              />
+            </Link>
+            <Link href="/">
+              <a className={utilStyles.boldText}>
+                SSGとSSRの使い分けの場面はいつなのか？
+              </a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>February 23,2023</small>
+          </article>
+          <article>
+            <Link href="/">
+              <img
+                src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+                alt=""
+              />
+            </Link>
+            <Link href="/">
+              <a className={utilStyles.boldText}>
+                SSGとSSRの使い分けの場面はいつなのか？
+              </a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>February 23,2023</small>
+          </article>
+          <article>
+            <Link href="/">
+              <img
+                src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+                alt=""
+              />
+            </Link>
+            <Link href="/">
+              <a className={utilStyles.boldText}>
+                SSGとSSRの使い分けの場面はいつなのか？
+              </a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>February 23,2023</small>
+          </article>
+          <article>
+            <Link href="/">
+              <img
+                src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+                alt=""
+              />
+            </Link>
+            <Link href="/">
+              <a className={utilStyles.boldText}>
+                SSGとSSRの使い分けの場面はいつなのか？
+              </a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>February 23,2023</small>
+          </article>
+        </div>
       </section>
     </Layout>
   );
