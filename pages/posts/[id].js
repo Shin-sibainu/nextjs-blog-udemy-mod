4eb8b288f6b4e556eec8e035c091c408a34a3e78
masaru suzuki <masaru.suzuki.yamagata@gmail.com>
2023-06-13 09:46:02 +0900

    add: 投稿データを表示できるようにした

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,26 @@
+import { useRouter } from 'next/router';
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/post';
+import utilStyle from '../../styles/utils.module.css';
 
 const Post = ({ post }) => {
-  return <Layout>{post.blogContentHtml}</Layout>;
+  // const router = useRouter();
+
+  // if (router.isFallback) {
+  //   return <div>Loading...</div>;
+  // }
+
+  return (
+    <Layout>
+      <article>
+        <h1 className={utilStyle.title}>{post.title}</h1>
+        <div className={utilStyle.lightText}>{post.date}</div>
+
+        {/* 記事本文 */}
+        <div dangerouslySetInnerHTML={{ __html: post.blogContentHtml }} />
+      </article>
+    </Layout>
+  );
 };
 
 export const getStaticPaths = async () => {
